5f9e944e647d074043b8b08201dd873e184cb6bd
Artem Borisovskiy <bytefu@gmail.com>
2011-05-15 20:09:37 +0300

    Updated copyrights and licence

--- LiquidCrystalExt.h
+++ LiquidCrystalExt.h
@@ -1,8 +1,22 @@
 /* This is header file for LiquidCrystalExt library which adds support
  * for pin drivers to LiquidCrystal library for Arduino, thus giving
- * an opportunity to use LCD connected, for example, to a shift register.
+ * an opportunity to use LCD connected, for example, through a shift register.
  *
  * (C) 2011 Artem Borisovskiy, bytefu@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #ifndef LIQUIDCRYSTALEXT_H
